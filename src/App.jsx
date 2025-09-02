06ad2046e55ffd7661788fe2bb21db6e5a222eaf
GondaliyaKishan <88085512+GondaliyaKishan@users.noreply.github.com>
2025-09-02 10:15:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from "./componets/counter";
+
+const App = () => {
+
+  const biodata ={
+    name :"harry",
+    age :"22"
+  }
+  return <div>
+   
+    <Counter name={biodata.name} age={biodata.age}/>
+  </div>
+};
+export default App;
---
+++ src/componets/counter.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Counter = (Counter) => {
+  return (
+    <h2>
+    
+      name :{Counter.name} <br /> Age :{Counter.age}
+    </h2>
+  );
+};
+export default Counter;
